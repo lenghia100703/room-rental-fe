843f3e51366e4a7af65b140b3ae0f4f202d5a7d3
Nam Huyy <101095378+NamHuyNguyenKhac@users.noreply.github.com>
2024-12-04 00:57:31 +0700

    Update Map.tsx

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,15 +1,15 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
 import './map.scss'
 import 'leaflet/dist/leaflet.css'
-import Pin from '@/components/pin/Pin.tsx'
+import Pin from '../pin/Pin'
 
 function Map({ items }) {
+    console.log([items[0]?.latitude, items[0]?.longitude])
     return (
-        <MapContainer center={[items[0]?.latitude, items[0]?.longitude]} zoom={12} scrollWheelZoom={false}
-                      className="map">
+        <MapContainer key={items[0]?.latitude} center={[items[0]?.latitude, items[0]?.longitude]} zoom={12} scrollWheelZoom={false} className="map">
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://tmdt.fimo.edu.vn/hot/{z}/{x}/{y}.png"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             {items.map(item => (
                 <Pin item={item} key={item.id} />
@@ -19,3 +19,4 @@ function Map({ items }) {
 }
 
 export default Map
+
