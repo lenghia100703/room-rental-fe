4a20cd216a71323d796103923895bc1be1648f89
Nam Huyy <101095378+NamHuyNguyenKhac@users.noreply.github.com>
2024-12-03 23:50:34 +0700

    Update Map.tsx

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -9,7 +9,7 @@ function Map({ items }) {
         <MapContainer center={[items[0]?.latitude, items[0]?.longitude]} zoom={12} scrollWheelZoom={false} className="map">
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                url="https://tmdt.fimo.edu.vn/hot/{z}/{x}/{y}.png"
             />
             {items.map(item => (
                 <Pin item={item} key={item.id} />
